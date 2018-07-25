022ec3146adc1754eb0635d49b527c40c26403d9
nicchagil <nic130330223@gmail.com>
2018-07-25 22:15:15 +0800

    使用ZooKeeper作为注册中心。

--- wc-user-core/src/main/java/com/nicchagil/util/dubbo/provider/UserDubboServiceImpl.java
+++ wc-user-core/src/main/java/com/nicchagil/util/dubbo/provider/UserDubboServiceImpl.java
@@ -1,13 +1,20 @@
 package com.nicchagil.util.dubbo.provider;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.nicchagil.util.dubbo.common.UserDubboInput;
 import com.nicchagil.util.dubbo.common.UserDubboOutput;
 import com.nicchagil.util.dubbo.common.UserDubboService;
 
 public class UserDubboServiceImpl implements UserDubboService {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Override
 	public UserDubboOutput getByCriteria(UserDubboInput userDubboInput) {
+		logger.info("被调用...");
+		
 		if (userDubboInput == null) {
 			throw new DubboRuntimeException("服务入参异常");
 		}
