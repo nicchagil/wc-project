e5353f8dec161e8e6f9c6267a0e45a105df7bb74
nicchagil <nic130330223@gmail.com>
2018-07-28 02:14:05 +0800

    通过配置文件配置dubbo的参数；添加DateTimeUtils。

--- wc-user-core/src/main/java/com/nicchagil/util/dubbo/DubboProperties.java
+++ wc-user-core/src/main/java/com/nicchagil/util/dubbo/DubboProperties.java
@@ -4,17 +4,84 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "dubbo.rpc")
+@ConfigurationProperties(prefix = "dubbo")
 public class DubboProperties {
+	
+	/** 注册中心协议 **/
+	private String registryProtocol;
+	
+	/** 注册中心地址 **/
+	private String registryAddress;
+	
+	/** 注册中心客户端 **/
+	private String registryClient;
+	
+	/** 服务提供者和消费者间的传输协议 **/
+	public String rpcProtocol;
 
-	private Integer port;
+	/** 服务提供者和消费者间的端口 **/
+	private Integer rpcPort;
+	
+	/** 服务提供者代理模式 **/
+	public String serviceProviderProxy;
+	
+	/** 服务版本 **/
+	public String serviceVersion;
 
-	public Integer getPort() {
-		return port;
+	public String getRegistryProtocol() {
+		return registryProtocol;
 	}
 
-	public void setPort(Integer port) {
-		this.port = port;
+	public void setRegistryProtocol(String registryProtocol) {
+		this.registryProtocol = registryProtocol;
 	}
 
+	public String getRegistryAddress() {
+		return registryAddress;
+	}
+
+	public void setRegistryAddress(String registryAddress) {
+		this.registryAddress = registryAddress;
+	}
+
+	public String getRegistryClient() {
+		return registryClient;
+	}
+
+	public void setRegistryClient(String registryClient) {
+		this.registryClient = registryClient;
+	}
+
+	public String getRpcProtocol() {
+		return rpcProtocol;
+	}
+
+	public void setRpcProtocol(String rpcProtocol) {
+		this.rpcProtocol = rpcProtocol;
+	}
+
+	public Integer getRpcPort() {
+		return rpcPort;
+	}
+
+	public void setRpcPort(Integer rpcPort) {
+		this.rpcPort = rpcPort;
+	}
+
+	public String getServiceProviderProxy() {
+		return serviceProviderProxy;
+	}
+
+	public void setServiceProviderProxy(String serviceProviderProxy) {
+		this.serviceProviderProxy = serviceProviderProxy;
+	}
+
+	public String getServiceVersion() {
+		return serviceVersion;
+	}
+
+	public void setServiceVersion(String serviceVersion) {
+		this.serviceVersion = serviceVersion;
+	}
+	
 }
--- wc-user-core/src/main/java/com/nicchagil/util/dubbo/config/DubboConfiguration.java
+++ wc-user-core/src/main/java/com/nicchagil/util/dubbo/config/DubboConfiguration.java
@@ -1,5 +1,7 @@
 package com.nicchagil.util.dubbo.config;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,8 +10,8 @@ import com.alibaba.dubbo.config.ApplicationConfig;
 import com.alibaba.dubbo.config.ProtocolConfig;
 import com.alibaba.dubbo.config.RegistryConfig;
 import com.alibaba.dubbo.config.spring.ServiceBean;
+import com.nicchagil.util.datetime.DateTimeUtils;
 import com.nicchagil.util.dubbo.DubboProperties;
-import com.nicchagil.util.dubbo.common.DubboCommonConstants;
 import com.nicchagil.util.dubbo.common.UserDubboService;
 import com.nicchagil.util.dubbo.provider.UserDubboServiceImpl;
 import com.nicchagil.util.spring.ApplicationContextUtils;
@@ -23,31 +25,31 @@ public class DubboConfiguration {
 	@Bean
 	public ApplicationConfig applicationConfig() {
 		ApplicationConfig applicationConfig = new ApplicationConfig();
-		applicationConfig.setName(DubboCommonConstants.A_PROVIDER_APPLICATION_NAME);
+		applicationConfig.setName("wc-user-" + DateTimeUtils.format(new Date(), DateTimeUtils.COHERENT_TIME));
 		return applicationConfig;
 	}
 	
 	@Bean
 	public RegistryConfig registryConfig() {
 		RegistryConfig registryConfig = new RegistryConfig();
-		registryConfig.setProtocol(DubboCommonConstants.MAIN_REGISTRY_PROTOCOL);
-		registryConfig.setAddress(DubboCommonConstants.MAIN_REGISTRY_ADDRESS);
-		registryConfig.setClient(DubboCommonConstants.MAIN_REGISTRY_CLIENT);
+		registryConfig.setProtocol(this.dubboProperties.getRegistryProtocol());
+		registryConfig.setAddress(this.dubboProperties.getRegistryAddress());
+		registryConfig.setClient(this.dubboProperties.getRegistryClient());
 		return registryConfig;
 	}
 	
 	@Bean
 	public ProtocolConfig protocolConfig() {
 		ProtocolConfig protocolConfig = new ProtocolConfig();
-		protocolConfig.setName(DubboCommonConstants.MAIN_RPC_PROTOCOL);
-		protocolConfig.setPort(this.dubboProperties.getPort());
+		protocolConfig.setName(this.dubboProperties.getRpcProtocol());
+		protocolConfig.setPort(this.dubboProperties.getRpcPort());
 		return protocolConfig;
 	}
 	
 	@Bean
 	public ServiceBean userDubboService() {
 		ServiceBean serviceBean = new ServiceBean();
-		serviceBean.setProxy(DubboCommonConstants.SERVICE_PROVIDER_PROXY);
+		serviceBean.setProxy(this.dubboProperties.serviceProviderProxy);
 		serviceBean.setInterface(UserDubboService.class);
 		serviceBean.setRef(ApplicationContextUtils.getBean(UserDubboServiceImpl.class));
 		serviceBean.setTimeout(1000);
