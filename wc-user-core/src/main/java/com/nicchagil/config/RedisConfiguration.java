1739ddaacfa41f5163abbbf26c4c8fba5b5f78b8
nicchagil <nic130330223@gmail.com>
2018-08-10 00:12:32 +0800

    添加redis开关。

--- wc-user-core/src/main/java/com/nicchagil/config/RedisConfiguration.java
+++ wc-user-core/src/main/java/com/nicchagil/config/RedisConfiguration.java
@@ -2,6 +2,7 @@ package com.nicchagil.config;
 
 import java.net.UnknownHostException;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
+@ConditionalOnProperty(name = "redisEnable", havingValue = "true") // 如果配置项“dubboEnable”的值与“havingValue”的值一致，则为true，Configuration生效；否则为false，Configuration不生效
 public class RedisConfiguration {
 
 	@Bean
