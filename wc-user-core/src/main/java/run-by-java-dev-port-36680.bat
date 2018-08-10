1fe5fc46d4f518c561f8ef7dcf856db45b3d6faa
nicchagil <nic130330223@gmail.com>
2018-08-11 00:34:05 +0800

    添加脚本逻辑：如果依赖的JAR包文件夹不存在，则调用mvn导出JAR。

--- wc-user-core/src/main/java/run-by-java-dev-port-36680.bat
+++ wc-user-core/src/main/java/run-by-java-dev-port-36680.bat
@@ -3,6 +3,14 @@ title APP-36680
 
 set dateTimeStr=%date:~0,4%-%date:~5,2%-%date:~8,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%
 
-# 设置CLASSPATH，执行此脚本请确认已执行本项目的“export-jar.bat”脚本下载所依赖的JAR
+::如果没有下载依赖的JAR，则下载
+if not exist ../dependency/ (
+	echo ../dependency/ not exist
+    cd ../../
+    call mvn dependency:copy-dependencies
+)
+
+::设置CLASSPATH，执行此脚本请确认已执行本项目的“export-jar.bat”脚本下载所依赖的JAR
+cd ./target/classes
 set CLASSPATH=../dependency/*;./classes;%CLASSPATH%
 java com.nicchagil.WcUserApplication
--- wc-user-core/src/main/java/run-by-java-dev-port-36681.bat
+++ wc-user-core/src/main/java/run-by-java-dev-port-36681.bat
@@ -1,6 +1,15 @@
 @echo off
 title APP-36681
 
+::如果没有下载依赖的JAR，则下载
+if not exist ../dependency/ (
+	echo ../dependency/ not exist
+    cd ../../
+    call mvn dependency:copy-dependencies
+)
+
+::设置CLASSPATH，执行此脚本请确认已执行本项目的“export-jar.bat”脚本下载所依赖的JAR
+cd ./target/classes
 set dateTimeStr=%date:~0,4%-%date:~5,2%-%date:~8,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%
 set CLASSPATH=../dependency/*;./classes;%CLASSPATH%
 java com.nicchagil.WcUserApplication --spring.profiles.active=dev-port-36681
