779170a629293d395d7abb48db7681795288f7d2
nicchagil <nic130330223@gmail.com>
2018-08-11 17:30:37 +0800

    将共用类提取到共用包

---
+++ dubbo-common/src/com/nicchagil/constants/RpcContextConstants.java
@@ -0,0 +1,7 @@
+package com.nicchagil.constants;
+
+public interface RpcContextConstants {
+	
+	String SESSION_ID_KEY = "SESSION_ID";
+
+}
---
+++ dubbo-common/src/com/nicchagil/user/interfaces/UserDubboService.java
@@ -0,0 +1,18 @@
+package com.nicchagil.user.interfaces;
+
+import com.nicchagil.user.model.UserDubboInput;
+import com.nicchagil.user.model.UserDubboOutput;
+
+public interface UserDubboService {
+	
+	/**
+	 * 根据条件查询记录
+	 */
+	UserDubboOutput getByCriteria(UserDubboInput userDubboInput);
+	
+	/**
+	 * 插入记录
+	 */
+	UserDubboOutput insert(UserDubboInput userDubboInput);
+
+}
