dbe988c631d50b3d3bd262c49724065ae8cce2f6
nicchagil <nic130330223@gmail.com>
2018-08-25 07:08:30 +0800

    支持Spring触发器和原生触发器。

--- wc-user/src/main/java/com/nicchagil/util/quartz/QuartzConfig.java
+++ wc-user/src/main/java/com/nicchagil/util/quartz/QuartzConfig.java
@@ -1,10 +1,13 @@
 package com.nicchagil.util.quartz;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.collections.CollectionUtils;
+import org.assertj.core.util.Arrays;
 import org.quartz.Trigger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +16,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+import com.google.common.collect.Lists;
 
 @Configuration
 public class QuartzConfig {
@@ -27,7 +32,7 @@ public class QuartzConfig {
      * 调度器工厂Bean
      */
     @Bean(name = "schedulerFactory")
-    public SchedulerFactoryBean schedulerFactory( Trigger... triggers) {
+    public SchedulerFactoryBean schedulerFactory(Trigger... fromSpringTriggers) {
         SchedulerFactoryBean bean = new SchedulerFactoryBean();
 
         Properties p = new Properties();
@@ -46,9 +51,60 @@ public class QuartzConfig {
         bean.setOverwriteExistingJobs(true);
         // 延时启动定时任务，避免系统未完全启动却开始执行定时任务的情况
         bean.setStartupDelay(15);
-        // 注册触发器
-        bean.setTriggers(triggers);
+        
+        /* 注册触发器 */
+        bean.setTriggers(this.mergeTriggers(fromSpringTriggers, this.getOriginalTriggers()));
+        
         return bean;
+    }
+    
+    /**
+     * 合并数据 
+     */
+    public Trigger[] mergeTriggers(Trigger[]... triggerArrays) {
+    	if (triggerArrays == null || triggerArrays.length == 0) {
+    		return new Trigger[] {};
+    	}
+    	
+    	List<Trigger> triggerList = Lists.newArrayList();
+    	for (Trigger[] triggerArray : triggerArrays) {
+    		if (Arrays.isNullOrEmpty(triggerArray)) {
+    			continue;
+    		}
+    		
+    		triggerList.addAll(Lists.newArrayList(triggerArray));
+    	}
+    	
+    	return this.toTriggerArray(triggerList);
+    }
+    
+    /**
+     * 获取继承Job的全部触发器
+     */
+    public Trigger[] getOriginalTriggers() {
+    	List<Trigger> triggerList = Lists.newArrayList();
+    	
+    	Trigger[] triggerArray = new Trigger[triggerList.size()];
+    	if (CollectionUtils.isNotEmpty(triggerList)) {
+    		triggerList.toArray(triggerArray);
+    	}
+    	
+    	return triggerArray;
+    }
+    
+    /**
+     * Trigger集合转换为Trigger数据
+     */
+    public Trigger[] toTriggerArray(List<Trigger> triggerList) {
+    	if (triggerList == null || triggerList.size() == 0) {
+    		return new Trigger[] {};
+    	}
+    	
+    	Trigger[] triggerArray = new Trigger[triggerList.size()];
+    	if (CollectionUtils.isNotEmpty(triggerList)) {
+    		triggerList.toArray(triggerArray);
+    	}
+    	return triggerArray;
     }
 
 }
