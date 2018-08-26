ebdbd403554d9379deba9389a3445b04ebec21eb
nicchagil <nic130330223@gmail.com>
2018-08-26 14:52:45 +0800

    添加Dubbo Token特性。

--- wc-user/src/main/java/com/nicchagil/util/dubbo/aop/RpcContextAop.java
+++ wc-user/src/main/java/com/nicchagil/util/dubbo/aop/RpcContextAop.java
@@ -1,18 +1,25 @@
 package com.nicchagil.util.dubbo.aop;
 
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.dubbo.rpc.RpcContext;
 
 @Aspect
 @Configuration /* 本来想用此AOP在调用提供者服务时隐式将SESSION ID传递过去，但因消费方无法通过结果在调用时拦截，需另寻他法。另一方面，觉得如需在提供者获取登录用户，因并不是任何提供者服务网都有此需求，由此需求的应用再显式传参也可 */
 public class RpcContextAop {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Value("${dubbo.token}")
+	private String dubboToken;
     
 	/* com.nicchagil.dubbo.interfaces包及其子包下的所有方法 */
     @Pointcut("execution(* com.nicchagil.dubbo.interfaces..*.*(..))")
@@ -23,6 +30,10 @@ public class RpcContextAop {
     	
     	Object target = proceedingJoinPoint.getTarget();
     	this.logger.info("AOP Object : {}, AOP Object Type : {}", target, target.getClass());
+    	
+    	if (StringUtils.isNotBlank(dubboToken) && !Boolean.FALSE.toString().equalsIgnoreCase(dubboToken)) {
+    		RpcContext.getContext().setAttachment("token", dubboToken);
+    	}
     	
     	Object result = proceedingJoinPoint.proceed();
     	return result;
--- wc-user/src/main/java/run-by-java-dev-port-36681.cmd
+++ wc-user/src/main/java/run-by-java-dev-port-36681.cmd
@@ -1,32 +1,23 @@
 @echo off
 title APP-36681
 
-::设置可读的时间戳
-::set dateTimeStr=%date:~0,4%-%date:~5,2%-%date:~8,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%
+@rem set dateTimeStr=%date:~0,4%-%date:~5,2%-%date:~8,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%
 
-::依赖的项目的根目录
 set dependent_project_a_directory=D:/git_workspace/wc-project/wc-all-dubbo-common
-::当前项目的根目录
 set current_project_directory=D:/git_workspace/wc-project/wc-user
 
-::安装依赖的项目（保证依赖的项目本地仓库安装的版本是最新的）
 cd %dependent_project_a_directory%
 call mvn clean install
 
-::如果没有下载依赖的JAR，则下载
 cd %current_project_directory%
 call mvn dependency:copy-dependencies
 
-::设置CLASSPATH，执行此脚本请确认已执行本项目的“export-jar.bat”脚本下载所依赖的JAR
 cd %current_project_directory%/target/classes
 
-::设置编译路径
 set CLASSPATH=../dependency/*;./classes;%CLASSPATH%
 
-::JAVA参数
 set vm_args=
 set vm_args=%vm_args% -Xms256m -Xmx256m -Xss512k
-::设置提供者直连
 set vm_args=%vm_args% -Ddubbo.resolve.file=%current_project_directory%/direct-provider.properties
 
 java %vm_args% com.nicchagil.WcUserApplication --spring.profiles.active=dev-port-36681
