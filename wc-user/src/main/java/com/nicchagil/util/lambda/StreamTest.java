5988f9e2c88a765da1ed3a2a4145e3d69bd3e285
nicchagil <nic130330223@gmail.com>
2018-09-02 11:21:54 +0800

    添加stream练习。

--- wc-user/src/main/java/com/nicchagil/util/lambda/StreamTest.java
+++ wc-user/src/main/java/com/nicchagil/util/lambda/StreamTest.java
@@ -3,10 +3,27 @@ package com.nicchagil.util.lambda;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 
 public class StreamTest {
+	
+	@Test
+	public void streamMethodTest() {
+		List<String> list = Arrays.asList(new String[] {"1", "2", "3"});
+		
+		Stream<String> stream = list.stream();
+		stream.allMatch(i -> i != null && i.equals("1"));
+		stream.anyMatch(i -> i != null && i.equals("1"));
+		stream.findFirst();
+		stream.collect(Collectors.toSet());
+		stream.collect(StringBuilder::new, StringBuilder::append, StringBuilder::append);
+		stream.count();
+		stream.distinct().collect(Collectors.toSet());
+		stream.filter(i -> i != null && i.equals("1"));
+		stream.findAny();
+	}
 	
 	@Test
 	public void countTest() {
