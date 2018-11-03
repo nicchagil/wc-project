4ee26848bad66905ec371238b73995885e4a5867
nicchagil <nic130330223@gmail.com>
2018-11-03 21:14:16 +0800

    SK功能开发。

--- wc-user/src/main/java/com/nicchagil/util/dubbo/aop/RpcContextAop.java
+++ wc-user/src/main/java/com/nicchagil/util/dubbo/aop/RpcContextAop.java
@@ -4,18 +4,16 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.util.Assert;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.nicchagil.util.dubbo.ext.args.signature.ArgsSignatureUtils;
 
-@Aspect
-@Configuration /* 本来想用此AOP在调用提供者服务时隐式将SESSION ID传递过去，但因消费方无法通过结果在调用时拦截，需另寻他法。另一方面，觉得如需在提供者获取登录用户，因并不是任何提供者服务网都有此需求，由此需求的应用再显式传参也可 */
+// @Aspect
+// @Configuration /* 本来想用此AOP在调用提供者服务时隐式将SESSION ID传递过去，但因消费方无法通过结果在调用时拦截，需另寻他法。另一方面，觉得如需在提供者获取登录用户，因并不是任何提供者服务网都有此需求，由此需求的应用再显式传参也可 */
 public class RpcContextAop {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
